df428b4fa9f3569ff9b92313d646a489c50026dd
vishanth ms <vishanthms@vishanths-MacBook-Air.local>
2025-11-18 13:38:22 +0530

    Initial microservice code

---
+++ app.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("Hello from Microservice running on EKS!"));
+app.listen(3000, () => console.log("Service running on port 3000"));
